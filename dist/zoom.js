500c1e6086fd50e08f629e06e1d2dac7bc2a2603
Nishanth Shanmugham <nishanths@utexas.edu>
2016-12-29 15:56:51 -0600

    Add updated dist

---
+++ dist/zoom.js
@@ -0,0 +1,266 @@
+/*
+ * Pure JavaScript-only implementation of zoom.js.
+ *
+ * Original preamble:
+ * zoom.js - It's the best way to zoom an image
+ * @version v0.0.2
+ * @link https://github.com/fat/zoom.js
+ * @license MIT
+ *
+ * This implementation has the same API as the original, but:
+ *
+ *   * No ctrl/meta click to open image.
+ *   * No support for data-original.
+ *   * May be less compatible with older browsers because ES6 and no vendor prefixes.
+ *   * Targets only <img> with data-action="zoom" attribute, not all elements with data-action="zoom".
+ *   * Zooming is allowed at smaller screen widths than allowed by the original (see zoom.OFFSET).
+ *   * Listen for the "click" event on img[data-action="zoom"] using capture (similar to the original),
+ *     but does not cancel the event after handling.
+ *
+ * Needs a related CSS file to work. See the original project for more info.
+ *
+ * The MIT License. Copyright © 2016 Nishanth Shanmugham.
+ */
+(_ => {
+    var zoom = Object.create(null);
+
+    zoom.current = null;
+    zoom.OFFSET = 0;
+    zoom.initialScrollPos = -1;
+    zoom.initialTouchPos = -1;
+
+    var windowWidth = _ => document.documentElement.clientWidth;
+    var windowHeight = _ => document.documentElement.clientHeight;
+    var elementWidth = elem => Math.max(elem.clientWidth, elem.offsetWidth, elem.scrollWidth);
+    var elementHeight = elem => Math.max(elem.clientHeight, elem.offsetHeight, elem.scrollHeight);
+
+    var elemOffset = elem => {
+        var rect = elem.getBoundingClientRect();
+        var docElem = document.documentElement;
+        var win = window;
+        return {
+            top: rect.top + win.pageYOffset - docElem.clientTop,
+            left: rect.left + win.pageXOffset - docElem.clientLeft
+        };
+    };
+
+    var once = (elem, type, handler) => {
+        var fn = e => {
+            e.target.removeEventListener(type, fn);
+            handler();
+        };
+        elem.addEventListener(type, fn);
+    };
+
+    zoom.setup = _ => {
+        var elems = document.querySelectorAll("img[data-action='zoom']");
+        for (var i = 0; i < elems.length; i++) {
+            elems[i].addEventListener("click", zoom.prepareZoom);
+        }
+    };
+
+    zoom.prepareZoom = e => {
+        if (document.body.classList.contains("zoom-overlay-open")) {
+            return;
+        }
+        if (e.target.width >= windowWidth() - zoom.OFFSET) {
+            return;
+        }
+
+        zoom.closeCurrent(true);
+
+        zoom.current = new ZoomImage(e.target);
+        zoom.current.zoom();
+
+        zoom.addCloseListeners();
+    };
+
+    zoom.closeCurrent = force => {
+        if (zoom.current == null) {
+            return;
+        }
+        if (force) {
+            zoom.current.dispose();
+        } else {
+            zoom.current.close();
+        }
+        zoom.removeCloseListeners();
+        zoom.current = null;
+    };
+
+    zoom.addCloseListeners = _ => {
+        document.addEventListener("scroll", zoom.handleScroll);
+        document.addEventListener("keyup", zoom.handleKeyup);
+        document.addEventListener("touchstart", zoom.handleTouchStart);
+        document.addEventListener("click", zoom.handleClick, true);
+    };
+
+    zoom.removeCloseListeners = _ => {
+        document.removeEventListener("scroll", zoom.handleScroll);
+        document.removeEventListener("keyup", zoom.handleKeyup);
+        document.removeEventListener("touchstart", zoom.handleTouchStart);
+        document.removeEventListener("click", zoom.handleClick, true);
+    };
+
+    zoom.handleScroll = _ => {
+        if (zoom.initialScrollPos == -1) {
+            zoom.initialScrollPos = window.pageYOffset;
+        }
+
+        var deltaY = Math.abs(zoom.initialScrollPos - window.pageYOffset);
+        if (deltaY >= 40) {
+            zoom.closeCurrent();
+        }
+    };
+
+    zoom.handleKeyup = e => {
+        if (e.keyCode == 27) {
+            zoom.closeCurrent();
+        }
+    };
+
+    zoom.handleTouchStart = e => {
+        var t = e.touches[0];
+        if (t == null) {
+            return;
+        }
+
+        initialTouchPos = t.pageY;
+        e.target.addEventListener("touchmove", zoom.handleTouchMove);
+    };
+
+    zoom.handleTouchMove = e => {
+        var t = e.touches[0];
+        if (t == null) {
+            return;
+        }
+
+        if (Math.abs(t.pageY - initialTouchPos) > 10) {
+            zoom.closeCurrent();
+            e.target.removeEventListener("touchmove", zoom.handleTouchMove);
+        }
+    };
+
+    zoom.handleClick = e => {
+        zoom.closeCurrent();
+    };
+
+    class Size {
+        constructor(w, h) {
+            this.w = w;
+            this.h = h;
+        }
+    }
+
+    class ZoomImage {
+        constructor(img) {
+            this.img = img;
+            this.preservedTransform = img.style.transform;
+            this.wrap = null;
+            this.overlay = null;
+        }
+
+        forceRepaint() {
+            var _ = this.img.offsetWidth; 
+            return;
+        }
+
+        zoom() {
+            var size = new Size(this.img.naturalWidth, this.img.naturalHeight);
+
+            this.wrap = document.createElement("div");
+            this.wrap.classList.add("zoom-img-wrap");
+            this.img.parentNode.insertBefore(this.wrap, this.img);
+            this.wrap.appendChild(this.img);
+
+            this.img.classList.add("zoom-img");
+            this.img.setAttribute("data-action", "zoom-out");
+
+            this.overlay = document.createElement("div");
+            this.overlay.classList.add("zoom-overlay");
+            document.body.appendChild(this.overlay);
+
+            this.forceRepaint();
+            var scale = this.calculateScale(size);
+
+            this.forceRepaint();
+            this.animate(scale);
+
+            document.body.classList.add("zoom-overlay-open");
+        }
+
+        calculateScale(size) {
+            var scrollTop = window.pageYOffset;
+            var maxScaleFactor = size.w / this.img.width;
+
+            var viewportWidth = (windowWidth() - zoom.OFFSET);
+            var viewportHeight = (windowHeight() - zoom.OFFSET);
+            var imageAspectRatio = size.w / size.h;
+            var viewportAspectRatio = viewportWidth / viewportHeight;
+
+            if (size.w < viewportWidth && size.h < viewportHeight) {
+                return maxScaleFactor;
+            } else if (imageAspectRatio < viewportAspectRatio) {
+                return (viewportHeight / size.h) * maxScaleFactor;
+            } else {
+                return (viewportWidth / size.w) * maxScaleFactor;
+            }
+        }
+
+        animate(scale) {
+            var imageOffset = elemOffset(this.img);
+            var scrollTop = window.pageYOffset;
+
+            var viewportX = (windowWidth() / 2);
+            var viewportY = scrollTop + (windowHeight() / 2);
+
+            var imageCenterX = imageOffset.left + (this.img.width / 2);
+            var imageCenterY = imageOffset.top + (this.img.height / 2);
+
+            var tx = viewportX - imageCenterX;
+            var ty = viewportY - imageCenterY;
+            var tz = 0;
+
+            var imgTr = `scale(${scale})`;
+            var wrapTr = `translate3d(${tx}px, ${ty}px, ${tz}px)`;
+
+            this.img.style.transform = imgTr;
+            this.wrap.style.transform = wrapTr;
+        }
+
+        dispose() {
+            if (this.wrap == null || this.wrap.parentNode == null) {
+                return;
+            }
+            this.img.classList.remove("zoom-img");
+            this.img.setAttribute("data-action", "zoom");
+
+            this.wrap.parentNode.insertBefore(this.img, this.wrap);
+            this.wrap.parentNode.removeChild(this.wrap);
+
+            document.body.removeChild(this.overlay);
+            document.body.classList.remove("zoom-overlay-transitioning");
+        }
+
+        close() {
+            document.body.classList.add("zoom-overlay-transitioning");
+            this.img.style.transform = this.preservedTransform;
+            if (this.img.style.length === 0) {
+                this.img.removeAttribute("style");
+            }
+            this.wrap.style.transform = "none";
+
+            once(this.img, "transitionend", _ => {
+                this.dispose();
+                // XXX(nishanths): remove class should happen after dispose. Otherwise,
+                // a new click event could fire and create a duplicate ZoomImage for
+                // the same <img> element.
+                document.body.classList.remove("zoom-overlay-open");
+            });
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", _ => {
+        zoom.setup();
+    });
+})();
